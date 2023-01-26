e5dd89900b753eaa4af7b64580a9148111bf602e
sbellina <shawn.bellina@gmail.com>
2023-01-26 08:55:00 -0300

    stage

--- src/App.vue
+++ src/App.vue
@@ -6,9 +6,17 @@
   import Campaigns from "./components/Campaigns/Campaigns.vue";
   import Footer from "./components/Footer.vue";
   import { useDarkMode } from "./stores/darkMode";
-  import { ref } from "vue";
+  import { ref, onMounted } from "vue";
 
   const darkMode = ref(useDarkMode());
+  const animClass = ref("wtf");
+
+  onMounted(() => {
+    setTimeout(function () {
+      animClass.value = "hide";
+      console.log("WELCOME: Carolingian Royalty Registery");
+    }, 3000);
+  });
 </script>
 
 <template>
@@ -19,4 +27,59 @@
   <div class="h-2 w-full bg-black dark:bg-white" />
   <Campaigns />
   <Footer />
+
+  <div :class="animClass">
+        <div>
+            <div class="logo_container">
+                <img alt="flag" class="logo" src="./assets/contra_logo_white.svg" />
+            </div>
+        </div>
+    </div>
 </template>
+
+<style scoped>
+.logo {
+    width: 30%
+}
+.logo_container {
+    display: flex;
+    align-items: flex-start;
+    justify-content: center;
+}
+.wtf {
+    width: 100%;
+    height: 100vh;
+    top: 0;
+    left: 0;
+    position: fixed;
+    display: flex;
+}
+
+.wtf div {
+    width: 100%;
+    height: 100%;
+    background: black;
+    animation: up 3s ease-in forwards;
+    transform: scaleY(0);
+    /* background: rgb(211, 34, 3); */
+}
+
+@keyframes up {
+
+    100% {
+        transform-origin: top;
+        /* transform: scaleY(0);*/
+        transform: translateY(100%);
+    }
+
+    0% {
+        transform-origin: bottom;
+        /* transform: scaleY(1); */
+        transform: translateY(-0%);
+    }
+}
+
+.hide {
+    visibility: hidden;
+}
+</style>
