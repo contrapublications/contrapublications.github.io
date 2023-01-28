69cdab1c2ec6f83dba7e0683ed5836b27c646095
sbellina <shawn.bellina@gmail.com>
2023-01-28 14:29:11 -0300

    stage

--- src/Data/menuItems.js
+++ src/Data/menuItems.js
@@ -2,22 +2,22 @@ const MenuItems  = Object.freeze({
 
 	'The Gray Zone': { 
 		'title':"The Gray Zone",
-		"titleLink":"http://menu_missions_link.com",
+		"titleLink":"https://contrapublications.substack.com/TheGrayZone",
 	},
 	
 	'Cocoon': { 
 		'title':"Cocoon",
-		"titleLink":"http://menue_columns_link.com",
+		"titleLink":"https://contrapublications.substack.com/Cocoon",
 	},
 
 	'Eye Candy': { 
 		'title':"Eye Candy",
-		"titleLink":"http://menue_columns_link.com",
+		"titleLink":"https://contrapublications.substack.com/EyeCandy",
 	},
 
 	'About Us': { 
 		'title':"About Us",
-		"titleLink":"http://menue_about_us_link.com",
+		"titleLink":"https://contrapublications.substack.com/about",
 	},
 
 	'Subscribe': { 
